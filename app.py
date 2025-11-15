c312220d13de94a11a6c1a0ceb653d084ac3f7db
tirumalasettiv <venkat.tsetti@gmail.com>
2025-11-14 22:29:48 -0500

    updated the libraries

--- app.py
+++ app.py
@@ -3,29 +3,18 @@ import streamlit as st
 st.set_page_config(page_title="Data Profiling App", layout="wide")
 
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+import streamlit as st
 from ydata_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
+from streamlit_ydata_profiling import st_profile_report
+
+
 
 with st.sidebar:
     uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
     if uploaded_file is not None:
         st.write('Mode of operation:')
         minimal  = st.checkbox('Minimal Report', value=False) 
-        display_mode = st.radio( 'Display Mode', ('Primary', 'Dark', 'Orange'))
-        
-        if display_mode == 'Dark':
-            dark_mode = True
-            orange_mode = False
-        elif display_mode == 'Orange':
-            dark_mode = False
-            orange_mode = True
-        else:
-            dark_mode = False
-            orange_mode = False
-        
+
         
 
 if uploaded_file is not None:
